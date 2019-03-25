0469f931e3b28195c00a4ca83893900e65f7f831
L200170002 <48064663+L200170002@users.noreply.github.com>
2019-03-26 00:48:41 +0700

    Add files via upload

---
+++ Modul_4/5.py
@@ -0,0 +1,23 @@
+#Nama : Sugiyo
+#NIM : L200170002
+#Kelas : A
+
+def binSe(list, target):
+    low = 0
+    high = len(list) - 1
+    while(low<=high):
+        mid = (low+high)//2
+        if(list[mid] == target):
+            return "target di index "+str(mid)
+        elif(target<list[mid]):
+            high = mid - 1
+        else:
+            low = mid +1
+    return "target tidak ditemukan"
+
+list = [2,4,6,9,12,27,39,46,59,77]
+target = 12
+print(binSe(list,target))
+list = [2,4,6,9,12,27,39,46,59,77]
+target = 133
+print(binSe(list,target))
